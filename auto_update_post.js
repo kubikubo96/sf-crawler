1d41396c95a6c8320cff90ac2647e345e8067913
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-09-02 13:20:30 +0700

    "update crawl"

--- auto_update_post.js
+++ auto_update_post.js
@@ -102,9 +102,9 @@ import "dotenv/config";
       console.log(" \n PUBLISH post " + number_id + ": " + post_ids[number_id]);
 
       //add category
-      await page.$eval("#in-category-1", (el) => el.click()); //Tổng hợp
+      await page.$eval("#in-category-1", (el) => el.click()); //Tin tức
       // await page.$eval("#in-category-4", (el) => el.click()); //Top kỳ thú
-      await page.$eval("#in-category-7", (el) => el.click()); //Thủ thuật hữu ích
+      // await page.$eval("#in-category-7", (el) => el.click()); //Thợ công nghệ
       // await page.$eval("#in-category-63", (el) => el.click()); //Đời sống
 
       //add tag 1
