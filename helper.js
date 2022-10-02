fc68027ff5ab71f3da2ea74fc71215d79ef4d551
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-02 15:10:53 +0700

    "update crawl"

--- helper.js
+++ helper.js
@@ -18,58 +18,50 @@ export async function saveData(data) {
 
 export function handleListPage() {
     let listPage = [];
+
     for (let i = 0; i < LIST_CRAWL.length; i++) {
+
+        let dataDefault = {
+            source: LIST_CRAWL[i].source,
+            elmLinkPost: LIST_CRAWL[i].elmLinkPost,
+            typeLinkPost: LIST_CRAWL[i].typeLinkPost,
+            elmTitle: LIST_CRAWL[i].elmTitle,
+            elmContent: LIST_CRAWL[i].elmContent,
+            elmLink: LIST_CRAWL[i].elmLink,
+            elmImage: LIST_CRAWL[i].elmImage,
+            elmSortContent: LIST_CRAWL[i].elmSortContent,
+            elmTagP: LIST_CRAWL[i].elmTagP,
+            elmTagQuote: LIST_CRAWL[i].elmTagQuote,
+            elmTagFigure: LIST_CRAWL[i].elmTagFigure,
+        };
+
         switch (LIST_CRAWL[i].source) {
             case 'dienmayxanh.com':
-                listPage = listPageDienMayXanh(listPage, LIST_CRAWL[i]);
+                listPage = listPageDienMayXanh(listPage, LIST_CRAWL[i], dataDefault);
                 break;
             case 'funix.edu.vn':
-                listPage = listPageFunix(listPage, LIST_CRAWL[i]);
+                listPage = listPageFunix(listPage, LIST_CRAWL[i], dataDefault);
         }
     }
 
     return listPage;
 }
 
-export function listPageDienMayXanh(listPage, itemI) {
+export function listPageDienMayXanh(listPage, itemI, dataDefault) {
     for (let j = 0; j < itemI.data.length; j++) {
-        const temp = {
-            url: itemI.url + itemI.data[j].path,
-            tag: itemI.data[j].tag,
-            source: itemI.source,
-            elmLinkPost: itemI.elmLinkPost,
-            typeLinkPost: itemI.typeLinkPost,
-            elmTitle: itemI.elmTitle,
-            elmContent: itemI.elmContent,
-            elmLink: itemI.elmLink,
-            elmImage: itemI.elmImage,
-            elmSortContent: itemI.elmSortContent,
-            elmTagP: itemI.elmTagP,
-            elmTagQuote: itemI.elmTagQuote,
-            elmTagFigure: itemI.elmTagFigure,
-        };
+        let temp = dataDefault;
+        temp.url = itemI.url + itemI.data[j].path;
+        temp.tag = itemI.data[j].tag;
         listPage.push(temp);
     }
     return listPage;
 }
 
-export function listPageFunix(listPage, ItemI) {
+export function listPageFunix(listPage, ItemI, dataDefault) {
     for (let j = 1; j < ItemI.max; j++) {
-        const temp = {
-            url: ItemI.url + j,
-            tag: ItemI.tag,
-            source: ItemI.source,
-            elmLinkPost: ItemI.elmLinkPost,
-            typeLinkPost: ItemI.typeLinkPost,
-            elmTitle: ItemI.elmTitle,
-            elmContent: ItemI.elmContent,
-            elmLink: ItemI.elmLink,
-            elmImage: ItemI.elmImage,
-            elmSortContent: ItemI.elmSortContent,
-            elmTagP: ItemI.elmTagP,
-            elmTagQuote: ItemI.elmTagQuote,
-            elmTagFigure: ItemI.elmTagFigure,
-        };
+        let temp = dataDefault;
+        temp.url = ItemI.url + j;
+        temp.tag = ItemI.tag;
         listPage.push(temp);
     }
     return listPage;
