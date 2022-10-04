97487f1e5070f0617cb3625fcc193e6daed9aea0
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-05 05:01:11 +0700

    "update crawl"

---
+++ temp.js
@@ -0,0 +1,8 @@
+export const DATA_INTERNAL_POST = [
+  {
+    key_start: "xóa dữ liệu bảng tạm",
+    space: 5,
+    key_end: "Windows 11",
+    url: "https://kungfucongnghe.com/cach-xoa-du-lieu-bang-tam-tren-windows-11-154609092022.htm",
+  },
+];
