2bc2c36a74dcdfd03f58d7a57a42e0303a4bd92a
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-05 05:03:05 +0700

    "update crawl"

--- constants.js
+++ constants.js
@@ -109,114 +109,114 @@ export const LIST_CRAWL = [
   //         },
   //     ],
   // },
-  {
-    url: 'https://www.bachhoaxanh.com/kinh-nghiem-hay/aj/Category/ListNews?pageIndex=0&pageSize=10&cateId=',
-    source: "bachhoaxanh.com",
-    elmLinkPost: "li.news > a",
-    typeLinkPost: 'path',
-    elmTitle: ".boxcontent h1.titlearticle",
-    elmContent: ".boxcontent",
-    elmLink: ".boxcontent a",
-    elmImage: ".boxcontent img",
-    elmSortContent: ".boxcontent h2",
-    elmTagP: ".boxcontent > p",
-    elmTagQuote: ".boxcontent > blockquote",
-    elmTagFigure: ".boxcontent > figure",
-    data: [
-      {
-        path: "2086",
-        tag: ["Món ngon mỗi ngày", "Công thức nấu ăn"],
-      },
-      {
-        path: "2110",
-        tag: ["Món ngon mỗi ngày", "Mẹo vặt bếp núc"],
-      },
-      {
-        path: "2087",
-        tag: ["Món ngon mỗi ngày", "Đặc sản vùng miền"],
-      },
-      {
-        path: "2090",
-        tag: ["Món ngon mỗi ngày", "Địa điểm ăn uống"],
-      },
-      {
-        path: "2102",
-        tag: ["Món ngon mỗi ngày", "Chế độ ăn uống"],
-      },
-      {
-        path: "2109",
-        tag: ["Mẹo vặt cuộc sống", "Mẹo vặt gia đình"],
-      },
-      {
-        path: "2110",
-        tag: ["Mẹo vặt cuộc sống", "Mẹo vặt bếp núc"],
-      },
-      {
-        path: "2111",
-        tag: ["Mẹo vặt cuộc sống", "Mẹo làm đẹp"],
-      },
-      {
-        path: "2112",
-        tag: ["Mẹo vặt cuộc sống", "Mẹo chi tiêu"],
-      },
-      {
-        path: "2113",
-        tag: ["Mẹo vặt cuộc sống", "Phong thủy"],
-      },
-      {
-        path: "2114",
-        tag: ["Mẹo vặt cuộc sống", "Mẹo vặt công nghệ"],
-      },
-      {
-        path: "2115",
-        tag: ["Mẹo vặt cuộc sống", "Thông tin cần biết"],
-      },
-      {
-        path: "2102",
-        tag: ["Sống khỏe", "Chế độ ăn uống"],
-      },
-      {
-        path: "2103",
-        tag: ["Sống khỏe", "Sống xanh"],
-      },
-      {
-        path: "2348",
-        tag: ["Sống khỏe", "Mẹ và bé"],
-      },
-      {
-        path: "2094",
-        tag: ["Làm đẹp", "Thời trang"],
-      },
-      {
-        path: "2095",
-        tag: ["Làm đẹp", "Dưỡng da"],
-      },
-      {
-        path: "2096",
-        tag: ["Làm đẹp", "Trang điểm"],
-      },
-      {
-        path: "2111",
-        tag: ["Làm đẹp", "Mẹo làm đẹp"],
-      },
-      {
-        path: "2089",
-        tag: ["Ăn gì chơi gì", "Địa điểm du lịch"],
-      },
-      {
-        path: "2090",
-        tag: ["Ăn gì chơi gì", "Địa điểm ăn uống"],
-      },
-      {
-        path: "2091",
-        tag: ["Ăn gì chơi gì", "Phim hay"],
-      },
-      {
-        path: "2092",
-        tag: ["Ăn gì chơi gì", "Nhạc hay"],
-      },
-    ],
-  },
+  // {
+  //   url: 'https://www.bachhoaxanh.com/kinh-nghiem-hay/aj/Category/ListNews?pageIndex=0&pageSize=10&cateId=',
+  //   source: "bachhoaxanh.com",
+  //   elmLinkPost: "li.news > a",
+  //   typeLinkPost: 'path',
+  //   elmTitle: ".boxcontent h1.titlearticle",
+  //   elmContent: ".boxcontent",
+  //   elmLink: ".boxcontent a",
+  //   elmImage: ".boxcontent img",
+  //   elmSortContent: ".boxcontent h2",
+  //   elmTagP: ".boxcontent > p",
+  //   elmTagQuote: ".boxcontent > blockquote",
+  //   elmTagFigure: ".boxcontent > figure",
+  //   data: [
+  //     {
+  //       path: "2086",
+  //       tag: ["Món ngon mỗi ngày", "Công thức nấu ăn"],
+  //     },
+  //     {
+  //       path: "2110",
+  //       tag: ["Món ngon mỗi ngày", "Mẹo vặt bếp núc"],
+  //     },
+  //     {
+  //       path: "2087",
+  //       tag: ["Món ngon mỗi ngày", "Đặc sản vùng miền"],
+  //     },
+  //     {
+  //       path: "2090",
+  //       tag: ["Món ngon mỗi ngày", "Địa điểm ăn uống"],
+  //     },
+  //     {
+  //       path: "2102",
+  //       tag: ["Món ngon mỗi ngày", "Chế độ ăn uống"],
+  //     },
+  //     {
+  //       path: "2109",
+  //       tag: ["Mẹo vặt cuộc sống", "Mẹo vặt gia đình"],
+  //     },
+  //     {
+  //       path: "2110",
+  //       tag: ["Mẹo vặt cuộc sống", "Mẹo vặt bếp núc"],
+  //     },
+  //     {
+  //       path: "2111",
+  //       tag: ["Mẹo vặt cuộc sống", "Mẹo làm đẹp"],
+  //     },
+  //     {
+  //       path: "2112",
+  //       tag: ["Mẹo vặt cuộc sống", "Mẹo chi tiêu"],
+  //     },
+  //     {
+  //       path: "2113",
+  //       tag: ["Mẹo vặt cuộc sống", "Phong thủy"],
+  //     },
+  //     {
+  //       path: "2114",
+  //       tag: ["Mẹo vặt cuộc sống", "Mẹo vặt công nghệ"],
+  //     },
+  //     {
+  //       path: "2115",
+  //       tag: ["Mẹo vặt cuộc sống", "Thông tin cần biết"],
+  //     },
+  //     {
+  //       path: "2102",
+  //       tag: ["Sống khỏe", "Chế độ ăn uống"],
+  //     },
+  //     {
+  //       path: "2103",
+  //       tag: ["Sống khỏe", "Sống xanh"],
+  //     },
+  //     {
+  //       path: "2348",
+  //       tag: ["Sống khỏe", "Mẹ và bé"],
+  //     },
+  //     {
+  //       path: "2094",
+  //       tag: ["Làm đẹp", "Thời trang"],
+  //     },
+  //     {
+  //       path: "2095",
+  //       tag: ["Làm đẹp", "Dưỡng da"],
+  //     },
+  //     {
+  //       path: "2096",
+  //       tag: ["Làm đẹp", "Trang điểm"],
+  //     },
+  //     {
+  //       path: "2111",
+  //       tag: ["Làm đẹp", "Mẹo làm đẹp"],
+  //     },
+  //     {
+  //       path: "2089",
+  //       tag: ["Ăn gì chơi gì", "Địa điểm du lịch"],
+  //     },
+  //     {
+  //       path: "2090",
+  //       tag: ["Ăn gì chơi gì", "Địa điểm ăn uống"],
+  //     },
+  //     {
+  //       path: "2091",
+  //       tag: ["Ăn gì chơi gì", "Phim hay"],
+  //     },
+  //     {
+  //       path: "2092",
+  //       tag: ["Ăn gì chơi gì", "Nhạc hay"],
+  //     },
+  //   ],
+  // },
   {
     url: 'https://funix.edu.vn/chia-se-kien-thuc/page/',
     max: 434,
