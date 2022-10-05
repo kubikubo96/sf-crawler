f7cb322534208f5fa39bf83b0adcf63086d7557d
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-05 22:10:52 +0700

    "update crawl"

--- helper.js
+++ helper.js
@@ -1,65 +1,66 @@
 import {LIST_CRAWL} from "./constants.js";
+//import {LIST_CRAWL} from "./data_bot.js";
 import axios from "axios";
 
 export async function saveData(data) {
-    try {
-        await axios
-            .post(process.env.HOST_API, data)
-            .then(function (response) {
-            })
-            .catch(function (error) {
-                console.log("\n ---  \n ERROR SAVE DATABASE \n ---  \n ");
-                console.log(error.response.data);
-            });
-    } catch (error) {
-        console.log(error)
-    }
+  try {
+    await axios
+      .post(process.env.HOST_API, data)
+      .then(function (response) {
+      })
+      .catch(function (error) {
+        console.log("\n ---  \n ERROR SAVE DATABASE \n ---  \n ");
+        console.log(error.response.data);
+      });
+  } catch (error) {
+    console.log(error)
+  }
 }
 
 export function handleListPage() {
-    let listPage = [];
-    for (let i = 0; i < LIST_CRAWL.length; i++) {
-        let dataDefault = {
-            source: LIST_CRAWL[i].source,
-            elmLinkPost: LIST_CRAWL[i].elmLinkPost,
-            typeLinkPost: LIST_CRAWL[i].typeLinkPost,
-            elmTitle: LIST_CRAWL[i].elmTitle,
-            elmContent: LIST_CRAWL[i].elmContent,
-            elmLink: LIST_CRAWL[i].elmLink,
-            elmImage: LIST_CRAWL[i].elmImage,
-            elmSortContent: LIST_CRAWL[i].elmSortContent,
-            elmTagP: LIST_CRAWL[i].elmTagP,
-            elmTagQuote: LIST_CRAWL[i].elmTagQuote,
-            elmTagFigure: LIST_CRAWL[i].elmTagFigure,
-        };
+  let listPage = [];
+  for (let i = 0; i < LIST_CRAWL.length; i++) {
+    let dataDefault = {
+      source: LIST_CRAWL[i].source,
+      elmLinkPost: LIST_CRAWL[i].elmLinkPost,
+      typeLinkPost: LIST_CRAWL[i].typeLinkPost,
+      elmTitle: LIST_CRAWL[i].elmTitle,
+      elmContent: LIST_CRAWL[i].elmContent,
+      elmLink: LIST_CRAWL[i].elmLink,
+      elmImage: LIST_CRAWL[i].elmImage,
+      elmSortContent: LIST_CRAWL[i].elmSortContent,
+      elmTagP: LIST_CRAWL[i].elmTagP,
+      elmTagQuote: LIST_CRAWL[i].elmTagQuote,
+      elmTagFigure: LIST_CRAWL[i].elmTagFigure,
+    };
 
-        switch (LIST_CRAWL[i].source) {
-            case 'dienmayxanh.com':
-            case 'bachhoaxanh.com':
-                listPage = listPageTypeA(listPage, LIST_CRAWL[i], dataDefault);
-                break;
-        }
+    switch (LIST_CRAWL[i].source) {
+      case 'dienmayxanh.com':
+      case 'bachhoaxanh.com':
+        listPage = listPageTypeA(listPage, LIST_CRAWL[i], dataDefault);
+        break;
     }
+  }
 
-    return listPage;
+  return listPage;
 }
 
 export function listPageTypeA(listPage, itemI, dataDefault) {
-    for (let j = itemI.data.length - 1; j >= 0; j--) {
-        let temp = {...dataDefault};
-        temp.url = itemI.url + itemI.data[j].path;
-        temp.tag = itemI.data[j].tag;
-        listPage.push(temp);
-    }
-    return listPage;
+  for (let j = itemI.data.length - 1; j >= 0; j--) {
+    let temp = {...dataDefault};
+    temp.url = itemI.url + itemI.data[j].path;
+    temp.tag = itemI.data[j].tag;
+    listPage.push(temp);
+  }
+  return listPage;
 }
 
 export function listPageTypeB(listPage, ItemI, dataDefault) {
-    for (let j = ItemI.max; j >= 1; j--) {
-        let temp = {...dataDefault};
-        temp.url = ItemI.url + j;
-        temp.tag = ItemI.tag;
-        listPage.push(temp);
-    }
-    return listPage;
+  for (let j = ItemI.max; j >= 1; j--) {
+    let temp = {...dataDefault};
+    temp.url = ItemI.url + j;
+    temp.tag = ItemI.tag;
+    listPage.push(temp);
+  }
+  return listPage;
 }
