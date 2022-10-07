31c3a0eaa0e5093672f54d962c45a9f0a322f9b7
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-07 19:08:30 +0700

    "update crawl"

--- constants.js
+++ constants.js
@@ -198,7 +198,7 @@ export const ELM_TRASH = [
   '.generate-productbox', '.generate-gallery', '.mce-preview-object', '.mce-object-iframe', 'p iframe', '.top-news', 'ins', 'box_above',
   '.adbro-sm', '.adbro-xs', 'adbro-bottom', '.mys-wrapper', '.adbro-satellite', '.adbro-animated', '.ez-toc-container', '.newscare',
   '#attachment_60985', '.kk-star-ratings', 'ul > li > a', '.ez-toc-title', '#player', '.html5-video-container', '.bxindexknh', 'tiktok',
-  'productbox', '.ad-even', 'p.title > p'
+  'productbox', '.ad-even', 'p.title > p', '.hiddenbxindex',
 
 ];
 
