36348fb3ac52045320be71265806e6ac08987b8a
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-16 23:50:45 +0700

    update crawl

--- auto_update.js
+++ auto_update.js
@@ -189,6 +189,11 @@ import {MINIMAL_ARGS} from "./minimal.js";
           console.log("\x1b[34m------------\x1b[0m\n");
           number_id = number_id + 1;
         }
+
+        //break while get list post
+        if (breakWhileGetListPost) {
+          break;
+        }
       } catch (error) {
         break;
       }
