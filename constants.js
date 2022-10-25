a7717aa08383b11a2f71748a4dee66922cd6e756
kubikubo96 <47631178+kubikubo96@users.noreply.github.com>
2022-10-25 15:07:42 +0700

    update crawl

--- constants.js
+++ constants.js
@@ -16,78 +16,78 @@ export const LIST_CRAWL = [
     data: [
       {
         path: "huong-dan-su-dung-laptop",
-        tag: ["Laptop"],
+        tag: ["Thợ công nghệ", "Laptop"],
       },
       {
         path: "huong-dan-su-dung",
-        tag: ["Điện thoại", "Hướng dẫn sự dụng điện thoại"],
+        tag: ["Thợ công nghệ", "Điện thoại"],
       },
       {
         path: "loi-ti-vi-thuong-gap",
-        tag: ["Tivi", "Lỗi tivi thường gặp"]
+        tag: ["Thợ công nghệ", "Tivi"]
       },
       {
         path: "tin-cong-nghe-dien-thoai",
-        tag: ["Điện thoại", "Tin công nghệ điện thoại", "Tin tức"]
+        tag: ["Tin tức", "Tin tức điện thoại"]
       },
       {
         path: "tin-cong-nghe-laptop",
-        tag: ["Laptop", "Tin công nghệ laptop", "Tin tức"]
+        tag: ["Tin tức", "Tin tức Laptop"]
       },
       {
         path: "huong-dan-su-dung-laptop",
-        tag: ["Laptop", "Hướng dẫn sử dụng laptop"]
+        tag: ["Thợ công nghệ", "Laptop"]
       },
       {
         path: "huong-dan-su-dung-may-tinh-bang",
-        tag: ["Máy tính bảng", "Hướng dẫn sử dụng máy tính bảng"]
+        tag: ["Thợ công nghệ", "Máy tính bảng"]
       },
       // {
       //   path: "meo-hay-facebook-zalo-youtube",
-      //   tag: ["Mạng xã hội", "Mẹo hay Facebook Zalo Youtube"]
+      //   tag: ["Hướng dẫn", "Mạng xã hội"]
       // },
       // {
       //   path: "tin-hoc-van-phong",
-      //   tag: ["Tin học văn phòng"]
+      //   tag: ["Hướng dẫn", "Tin học văn phòng"]
       // },
       // {
       //   path: "phu-kien",
-      //   tag: ["Phụ kiện"]
+      //   tag: ["Hướng dẫn", "Phụ kiện"]
       // },
       // {
       //   path: "thuong-hieu-dong-ho",
-      //   tag: ["Đồng hồ", "Thương hiệu đồng hồ"]
+      //   tag: ["Tin tức", "Thương hiệu đồng hồ"]
       // },
       // {
       //   path: "meo-huong-dan-su-dung-dong-ho",
-      //   tag: ["Đồng hồ", "Mẹo hướng dẫn sử dụng dồng hồ"]
+      //   tag: ["Hướng dẫn", "Mẹo hướng dẫn sử dụng dồng hồ"]
       // },
       // {
       //   path: "thuat-ngu-dong-ho",
-      //   tag: ["Đồng hồ", "Thuật ngữ đồng hồ"]
+      //   tag: ["Hướng dẫn", "Thuật ngữ đồng hồ"]
       // },
       // {
       //   path: "phan-biet-dong-ho-that-gia",
-      //   tag: ["Đồng hồ", "Phân biệt đồng hồ thật giả"]
+      //   tag: ["Hướng dẫn", "Phân biệt đồng hồ thật giả"]
       // },
       // {
       //   path: "thuong-hieu-mat-kinh",
-      //   tag: ["Mắt kính", "Thương hiệu mắt kính"]
+      //   tag: ["Tin tức", "Thương hiệu mắt kính"]
       // },
       // {
       //   path: "thiet-bi-thong-minh-phong-ngu",
-      //   tag: ["Thiết bị thông minh", "Thiết bị thông minh phòng ngủ"]
+      //   tag: ["Hướng dẫn", "Thiết bị thông minh"]
       // },
       // {
       //   path: "thiet-bi-thong-minh-phong-khach",
-      //   tag: ["Thiết bị thông minh", "Thiết bị thông minh phòng khách"]
+      //   tag: ["Hướng dẫn", "Thiết bị thông minh"]
       // }, {
       //   path: "thiet-bi-thong-minh-nha-bep",
-      //   tag: ["Thiết bị thông minh", "Thiết bị thông minh nhà bếp"]
+      //   tag: ["Hướng dẫn", "Thiết bị thông minh"]
       // },
       // {
       //   path: "thiet-bi-thong-minh-khac",
-      //   tag: ["Thiết bị thông minh", "Thiết bị thông minh khác"]
+      //   tag: ["Hướng dẫn", "Thiết bị thông minh"]
       // },
     ],
   },
